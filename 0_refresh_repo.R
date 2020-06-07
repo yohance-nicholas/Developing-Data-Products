c48c4f383ef4707e4b746cf0d6c51477455b9a73
yohance-nicholas <57534959+yohance-nicholas@users.noreply.github.com>
2020-06-07 14:07:34 -0400

    Update

---
+++ 0_refresh_repo.R
@@ -0,0 +1,8 @@
+# Refresh Developing Data Products Repo
+# Refresh Data
+source("https://git.io/JfMbR", local = T)
+# Render Presentations
+library(rmarkdown)
+rmarkdown::render("1_R_Markdown_and_Leaflet.Rmd")
+rmarkdown::render("2_R_Markdown_Presentation.Rmd")
+rmarkdown::render("3_Shiny_Application_and_Reproducible_Pitch.Rmd")
